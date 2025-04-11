909f14547cbe9c9233902556e30555631678c45a
achmad risky <89742078+achmadrisky@users.noreply.github.com>
2025-04-11 17:44:23 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true, // <-- tambahkan ini
   },
-  output: "export",
+  
 };
 
 export default nextConfig;
